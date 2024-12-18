93761ca4863af98295d2a3969af00ca70e366d89
Ziad Allam <ziadmahmoudallam@gmail.com>
2024-12-18 22:09:44 +0200

    CheckAuth update

--- admin-app/src/components/common/CheckAuth.jsx
+++ admin-app/src/components/common/CheckAuth.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { checkAuth } from '../../features/auth-admin/authSlice'
+import { toast } from 'react-toastify';
+
+const dispatch = useDispatch()
+useEffect(() => {
+    dispatch(checkAuth()).catch(() => {
+        toast.success("User is not authenticated. Redirecting to login...")
+    });
+}, [dispatch]);
+
 
 function CheckAuth({ isAuthenticated, user, children }) {
 
